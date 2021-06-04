5fc6c185f85e71dc6b06f125e16eb1dc1953aca4
Yujin Yano <northwindapps@gmail.com>
2021-06-04 22:50:59 +0900

    formatted

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const server = express();
 const port = 3000;
 
-server.use(express.urlencoded({extended: true}));
+server.use(express.urlencoded({ extended: true }));
 server.use(express.json());
 
 server.get('/products', async (req, res) => {
@@ -19,91 +19,73 @@ server.get('/prices', async (req, res) => {
     res.send(allprices);
 });
 
-server.get('/products/:id',async(req,res) => {
+server.get('/products/:id', async (req, res) => {
     var productId = null;
-        productId = req.params.id;
+    productId = req.params.id;
     const product = await stripe.products.retrieve(
         productId
     );
     res.send(product);
 });
 
- //single cutomer, show
- server.get('/customers/:id', async (req,res) => {
-    try{
+//single cutomer, show
+server.get('/customers/:id', async (req, res) => {
+    try {
         var customer = await stripe.customers.retrieve(req.params.id);
-    }catch(err){
-        if(err.statusCode == 404){
+    } catch (err) {
+        if (err.statusCode == 404) {
             res.sendStatus(err.statusCode);
         }
     }
     res.send(customer);
 });
 
-
- //single product, update
- server.post('/products', async (req,res) => {
+//single product, update
+server.post('/products', async (req, res) => {
     let newskus = req.body.skus;
     var products = req.body.productids;
-    try{
+    try {
         products.forEach(updateProduct);
-    }catch(err){
+    } catch (err) {
         console.log(err);
         res.sendStatus(err.statusCode);
     }
 
-    async function updateProduct(element,idx) {
-        product = await stripe.products.update(products[idx],{metadata:{sku:newskus[idx]}});
+    async function updateProduct(element, idx) {
+        product = await stripe.products.update(products[idx], { metadata: { sku: newskus[idx] } });
     }
- 
+
     res.sendStatus(200);
 });
 
-
-//forget orders api. use checkout instead for one-time purchase.
-// checkout complys with eu standards.
-//https://stripe.com/docs/payments/checkout not for mobile app
-// https://stripe.com/docs/api/checkout/sessions not for mobile app
-// https://stripe.com/docs/api/payment_intents This is the only option.
-// https://stripe.com/docs/api/products
-// server.get('/skus', async (req, res) => {
-//     //https://stripe.com/docs/api/products/list
-//     let skus = await stripe.skus.list({});
-//     res.send(skus);
-// });
-
-//new official
 server.post('/checkout', async (req, res) => {
     // Use an existing Customer ID if this is a returning customer.
     let products = req.body.products;
     let prices = req.body.prices;
     let quantities = req.body.quantities;
-    
 
     //stock shorting check
     quantities.forEach(checkitemstock);
-    async function checkitemstock (element,idx) {
-        if (quantities[idx]>0) {
-            try{
+    async function checkitemstock(element, idx) {
+        if (quantities[idx] > 0) {
+            try {
                 let product = await stripe.products.retrieve(
                     products[idx]
                 );
                 let sku = product["metadata"]["sku"];
                 let checkStock = parseInt(sku) - quantities[idx];
-            if (checkStock < 0) {
-                res.sendStatus(200);
-                res.send('Some items are out of stock.Sorry.');
-                return 
-            }   
-            }catch(err){
+                if (checkStock < 0) {
+                    res.sendStatus(200);
+                    res.send('Some items are out of stock.Sorry.');
+                    return
+                }
+            } catch (err) {
                 console.log(err);
                 res.sendStatus(err.statusCode);
             }
         }
     }
 
-    //TODO Update sku number before proceed the following process.
-    //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce
     let total_amount = products.reduce(calculate, 0, 0);
     function calculate(sum, num, idx) {
         var thePrice = prices[idx];
@@ -114,16 +96,12 @@ server.post('/checkout', async (req, res) => {
         return sum + theMultiplied;
     }
 
-    console.log("total",total_amount);
-
     var customerId = req.body.customerId;
-
-
     var customer = null;
-    try{
+    try {
         customer = await stripe.customers.retrieve(customerId);
-    }catch(err){
-        if(err.statusCode == 404){
+    } catch (err) {
+        if (err.statusCode == 404) {
         }
     }
 
@@ -131,30 +109,28 @@ server.post('/checkout', async (req, res) => {
         customer = await stripe.customers.create();
     }
 
-   
-
     const ephemeralKey = await stripe.ephemeralKeys.create(
-      {customer: customer.id},
-      {apiVersion: '2020-08-27'}
+        { customer: customer.id },
+        { apiVersion: '2020-08-27' }
     );
 
-    try{
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount: total_amount,
-      currency: 'usd',
-      customer: customer.id,
-      description: 'none'
-    });
-    res.json({
-      paymentIntent: paymentIntent.client_secret,
-      ephemeralKey: ephemeralKey.secret,
-      customer: customer.id
-    });
-    }catch(err){
+    try {
+        const paymentIntent = await stripe.paymentIntents.create({
+            amount: total_amount,
+            currency: 'usd',
+            customer: customer.id,
+            description: 'none'
+        });
+        res.json({
+            paymentIntent: paymentIntent.client_secret,
+            ephemeralKey: ephemeralKey.secret,
+            customer: customer.id
+        });
+    } catch (err) {
         res.sendStatus(err.statusCode);
     }
-  });
+});
 
 server.listen(process.env.PORT || port, () => {
-    console.log(`Example a:: listening at htttp://localhost:${process.env.PORT}`);
+    console.log(`Example a:: listening at htttp://localhost:${port}`);
 })
